6875d488b7366ca2d7926cf02f98ff69e72f6ab8
Ishida Yuta <ishida.yuta@kk.jp.panasonic.com>
2017-02-06 13:08:25 +0900

    fix base app

--- app/src/main/java/com/avcmms/android/internship/irrooster/view/IRkitControlActivity.java
+++ app/src/main/java/com/avcmms/android/internship/irrooster/view/IRkitControlActivity.java
@@ -1,16 +1,15 @@
-package com.avcmms.android.internship.irmonkey.view;
+package com.avcmms.android.internship.irrooster.view;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 
-import com.avcmms.android.internship.irmonkey.R;
-import com.avcmms.android.internship.irmonkey.model.IRkitControlloer;
+import com.avcmms.android.internship.irrooster.R;
+import com.avcmms.android.internship.irrooster.model.IRkitControlloer;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+public class IRkitControlActivity extends AppCompatActivity implements View.OnClickListener {
 
     private IRkitControlloer mIrkitController;
 
@@ -18,8 +17,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         mIrkitController = new IRkitControlloer(getApplicationContext());
 
         ((Button) findViewById(R.id.btn_cnl1)).setOnClickListener(this);
@@ -34,18 +31,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         ((Button) findViewById(R.id.btn_cnl10)).setOnClickListener(this);
         ((Button) findViewById(R.id.btn_cnl11)).setOnClickListener(this);
         ((Button) findViewById(R.id.btn_cnl12)).setOnClickListener(this);
-
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
-    public void onClick(View v) {
-        mIrkitController.sendCommand(v.getId());
+    protected void onPause() {
+        super.onPause();
     }
+
+    @Override
+    public void onClick(final View v) {
+        if (mIrkitController != null) {
+            mIrkitController.sendCommand(mIrkitController.getChanelId(v.getId()));
+        }
+    }
+
 }
